af60fb8c51662e362b069ca3b38116f439d0bc73
annbuiii <builehoaian2002@gmail.com>
2024-03-13 16:06:57 +0700

    Update UI create room screen

--- composeApp/src/commonMain/kotlin/com/anbui/skribbl/feature/createRoom/CreateRoomScreen.kt
+++ composeApp/src/commonMain/kotlin/com/anbui/skribbl/feature/createRoom/CreateRoomScreen.kt
@@ -1,33 +1,35 @@
 package com.anbui.skribbl.feature.createRoom
 
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalFocusManager
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.anbui.skribbl.core.presentation.component.SkribblColumn
 import com.anbui.skribbl.core.presentation.component.SkribblTextButton
 import com.anbui.skribbl.core.presentation.component.SkribblTextField
 import com.anbui.skribbl.core.presentation.theme.SpaceLarge
 import com.anbui.skribbl.core.presentation.theme.SpaceMedium
+import org.jetbrains.compose.resources.stringResource
+import skribbl.composeapp.generated.resources.Res
+import skribbl.composeapp.generated.resources.create
+import skribbl.composeapp.generated.resources.create_room
 
 class CreateRoomScreen : Screen {
     @Composable
     override fun Content() {
-        val localFocusManager = LocalFocusManager.current
         val screenModel: CreateRoomScreenModel = getScreenModel()
         val navigator = LocalNavigator.currentOrThrow
 
@@ -42,42 +44,53 @@ class CreateRoomScreen : Screen {
             }
         }
 
-        Column(
-            modifier = Modifier.pointerInput(Unit) {
-                detectTapGestures(onTap = {
-                    localFocusManager.clearFocus()
-                })
-            },
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        CreateRoomScreenContent(
+            roomName = roomName,
+            roomSize = roomSize,
+            onChangeRoomName = screenModel::changeRoomName,
+            onChangeRoomSize = screenModel::changeRoomSize,
+            onCreateRoom = screenModel::createRoom
+        )
+    }
+}
 
+@Composable
+fun CreateRoomScreenContent(
+    roomName: String,
+    roomSize: String,
+    onChangeRoomName: (String) -> Unit,
+    onChangeRoomSize: (String) -> Unit,
+    onCreateRoom: () -> Unit
+) {
+    SkribblColumn(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            stringResource(Res.string.create_room),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(SpaceLarge),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(SpaceMedium)
         ) {
-            Text("Create new room")
-            Column(
-                modifier = Modifier.fillMaxWidth().padding(SpaceLarge),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(SpaceMedium)
+            SkribblTextField(
+                modifier = Modifier.fillMaxWidth(),
+                text = roomName,
+                onValueChange = onChangeRoomName
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(SpaceMedium),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 SkribblTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = roomName,
-                    onValueChange = screenModel::changeRoomName
+                    modifier = Modifier.weight(1f),
+                    text = roomSize,
+                    onValueChange = onChangeRoomSize
                 )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(SpaceMedium),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    SkribblTextField(
-                        modifier = Modifier.weight(1f),
-                        text = roomSize,
-                        onValueChange = screenModel::changeRoomSize
-                    )
-
-                    SkribblTextButton("Create") {
-                        screenModel.createRoom()
-                    }
-                }
+                SkribblTextButton(text = stringResource(Res.string.create), onClick = onCreateRoom)
             }
         }
     }
